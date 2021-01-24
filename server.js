67d270860ea86b5e4c7ea493e8f487df85aaf3c3
daniel.monroy <daniel.monroym@udea.edu.co>
2021-01-24 14:18:27 -0500

    added documentation to / route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
@@ -24,6 +25,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Mount routers
 app.use("/api/v1/products", products);
 app.use("/api/v1/popularproducts", popularProducts);
